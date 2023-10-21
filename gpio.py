05f7b9160fa146453a9c127d4bf173a1958364fc
Milosz <inzynier.matuszewski@gmail.com>
2023-10-21 14:59:31 +0200

    first commit

---
+++ gpio.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+output_1 = 2
+output_2 = 3
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(output_1, GPIO.OUT) 
+GPIO.setup(output_2, GPIO.OUT) 
+
+# Initial state
+GPIO.output(output_1, GPIO.HIGH)
+GPIO.output(output_2, GPIO.HIGH)
+
+
+userInputPort = ''
+userInputState = ''
+
+dict ports = {'1': output_1, '2': output_2}
+dict states = {'on': GPIO.LOW, 'off': GPIO.HIGH}
+
+while (true):
+    userInputPort = input('Which port? 1 or 2 (or quit): ')
+    userInputState = input('What state (ON / OFF)? (or quit): ')
+
+    print('Changing port ', userInputPort, ' to ', userInputState)
+    GPIO.output(ports[userInputPort], states[userInputState])
+
+
+
