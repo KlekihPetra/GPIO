a479dde5ab14b3af13771904d3a9c627119990d7
Milosz <inzynier.matuszewski@gmail.com>
2023-12-09 17:46:54 +0100

    Add driveway gate script

---
+++ gate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# External module imports
+import RPi.GPIO as GPIO
+import datetime
+
+pulseLength = datetime.timedelta(seconds = 5)
+
+# Pin Definitons (Broadcom pin-numbering scheme):
+drivewayGatePin = 3
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(drivewayGatePin, GPIO.OUT) 
+
+# Initial state
+GPIO.output(drivewayGatePin, GPIO.HIGH)
+
+# Open gate (5s pulse)
+timerStart = datetime.datetime.now()
+
+print('Pulse start')
+GPIO.output(drivewayGatePin, GPIO.LOW)
+
+while (datetime.datetime.now() < timerStart + pulseLength):
+    continue
+
+print('Pulse end')
+GPIO.output(drivewayGatePin, GPIO.HIGH)
+
+
